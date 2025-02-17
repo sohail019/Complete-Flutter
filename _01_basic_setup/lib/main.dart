317014f082bc31222dd43f7728fde1c5e94be728
sohail019 <shaikhhsohail0193@gmail.com>
2025-02-17 13:28:22 +0530

    Refactor main.dart to use GradientContainer widget for improved code organization

--- _01_basic_setup/lib/main.dart
+++ _01_basic_setup/lib/main.dart
@@ -5,31 +5,41 @@ void main() {
     MaterialApp(
       home: Scaffold(
         // backgroundColor: Colors.greenAccent,
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.red,
-                // Colors.blue,
-                Colors.white,
-                Colors.green,
-                // Colors.yellow
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              "Hello Sohail",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 30,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+// Understanding classes
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.red,
+            // Colors.blue,
+            Colors.white,
+            Colors.green,
+            // Colors.yellow
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          "Hello Sohail",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 30,
+          ),
+        ),
+      ),
+    );
+  }
 }
