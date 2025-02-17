be822cc99f267825f12de4805bf2f2e05b65737d
sohail019 <shaikhhsohail0193@gmail.com>
2025-02-17 13:37:54 +0530

    Refactor GradientContainer to include StyledText widget for enhanced text styling

--- _01_basic_setup/lib/gradient_container.dart
+++ _01_basic_setup/lib/gradient_container.dart
@@ -7,27 +7,35 @@ class GradientContainer extends StatelessWidget {
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Colors.red,
-            // Colors.blue,
-            Colors.white,
-            Colors.green,
-            // Colors.yellow
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          "Hello Sohail",
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 30,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.red,
+              // Colors.blue,
+              Colors.white,
+              Colors.green,
+              // Colors.yellow
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
           ),
         ),
+        child: StyledText());
+  }
+}
+
+// Custom Widget for styled text\
+
+class StyledText extends StatelessWidget {
+  const StyledText({super.key});
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Text(
+        "Hello Sohail Shaikh!",
+        style: TextStyle(
+            fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black),
       ),
     );
   }
