8478a242c3998810a64865438aa5e5c9d7516077
sohail019 <shaikhhsohail0193@gmail.com>
2025-02-17 12:49:09 +0530

    Add additional project files and configuration for multiple architectures

--- _01_basic_setup/lib/main.dart
+++ _01_basic_setup/lib/main.dart
@@ -2,12 +2,24 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.greenAccent,
-        
-        body: Center(
-          child: Text("Hello Sohail"),
+        // backgroundColor: Colors.greenAccent,
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.red,
+                // Colors.blue,
+                Colors.white,
+                Colors.green,
+                // Colors.yellow
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Text("Hello Sohail"),
+          ),
         ),
       ),
     ),
