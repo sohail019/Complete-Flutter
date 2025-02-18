8e90a73d78744fa5edd249bfc8b3ae205e1ca924
sohail019 <shaikhhsohail0193@gmail.com>
2025-02-18 19:44:46 +0530

    Add quiz functionality with question data and answer buttons

--- _02_quiz_app/lib/questions_screen.dart
+++ _02_quiz_app/lib/questions_screen.dart
@@ -1,4 +1,6 @@
+import 'package:_02_quiz_app/answer_button.dart';
 import 'package:flutter/material.dart';
+import 'package:_02_quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,6 +12,28 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text("Questions Screen");
+    final currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: TextStyle(color: Colors.white, fontSize: 22),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.answers.map((answer) {
+              return AnswerButton(onTap: () {}, answerText: answer);
+            }),
+          ],
+        ),
+      ),
+    );
   }
 }
