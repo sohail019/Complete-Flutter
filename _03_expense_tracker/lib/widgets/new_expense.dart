e3cf6b0e1fa533c3efe1abdc427ed97b7207d58b
sohail019 <shaikhhsohail0193@gmail.com>
2025-02-21 15:49:09 +0530

    Refactor NewExpense widget: replace title input handling with TextEditingController, add amount input field, and enhance UI with clear buttons

--- _03_expense_tracker/lib/widgets/new_expense.dart
+++ _03_expense_tracker/lib/widgets/new_expense.dart
@@ -9,10 +9,21 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpenseState extends State<NewExpense> {
 
-  var _enteredTitle = '';
+  // var _enteredTitle = '';
 
-  void _saveTitleInput(String inputValue){
-    _enteredTitle = inputValue;
+  // void _saveTitleInput(String inputValue){
+  //   _enteredTitle = inputValue;
+  // }
+
+  final _titleController = TextEditingController(); //? creates an object optimized for text input
+  final _amountController = TextEditingController();
+
+  //? delete the controller when the widget is removed from the widget tree
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
   }
   @override
   Widget build(BuildContext context) {
@@ -21,17 +32,37 @@ class _NewExpenseState extends State<NewExpense> {
       child: Column(
         children: [
           TextField(
-            onChanged: _saveTitleInput,
+            controller: _titleController,
             maxLength: 50,
-            // keyboardType: TextInputType.text,
-            decoration: const InputDecoration(
-              label: Text("Title")
+            keyboardType: TextInputType.text,
+            decoration: InputDecoration(
+              label: Text("Title"),
+              suffix: IconButton(
+                onPressed: () => _titleController.clear(),
+                icon: Icon(Icons.clear),
+              ),
+            ),
+          ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              label: const Text("Amount"),
+              prefixText: "₹",
+              suffix: IconButton(
+                onPressed: () => _amountController.clear(),
+                icon: const Icon(Icons.clear),
+              ),
             ),
           ),
           Row(
             children: [
+              TextButton(onPressed: () {
+                Navigator.pop(context);
+              }, child: const Text("Cancel")),
               ElevatedButton(onPressed: () {
-                print(_enteredTitle);
+                print(_titleController.text);
+                print(_amountController.text);
               }, child: const Text("Add Expense")),
             ],
           )
