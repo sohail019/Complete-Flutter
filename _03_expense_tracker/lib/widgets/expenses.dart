5214b1e6c60232ce3e6bad10387e432afa9f8976
sohail019 <shaikhhsohail0193@gmail.com>
2025-02-24 12:35:25 +0530

    Implement expense removal with undo functionality and improve empty state handling in Expenses widget

--- _03_expense_tracker/lib/widgets/expenses.dart
+++ _03_expense_tracker/lib/widgets/expenses.dart
@@ -42,13 +42,39 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
     setState(() {
       _registeredExpenses.remove(expense);
     });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text("Expense removed!"),
+        action: SnackBarAction(
+          label: "Undo",
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text("No expenses added yet! Start adding some..."),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text('Expense Tracker by Sohail'),
@@ -63,12 +89,7 @@ class _ExpensesState extends State<Expenses> {
         children: [
           // Toolbar with the title and to add new expenses
           const Text('The Chart!'),
-          Expanded(
-            child: ExpensesList(
-              expenses: _registeredExpenses,
-              onRemoveExpense: _removeExpense,
-            ),
-          ),
+          Expanded(child: mainContent),
         ],
       ),
     );
