819489129c317d27fd5ae35341849b1fdcbe90da
sohail019 <shaikhhsohail0193@gmail.com>
2025-02-24 19:36:23 +0530

    Add MealsScreen and Meal model; implement meal management features

---
+++ _04_get_meals_app/lib/screens/meals.dart
@@ -0,0 +1,13 @@
+import 'package:_04_get_meals_app/models/meal.dart';
+import 'package:flutter/material.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({super.key, required this.title, required this.meals});
+
+  final String title;
+  final List<Meal> meals;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text(title)), body: Text("Hello"));
+  }
+}
--- _04_get_meals_app/lib/widgets/category_grid_item.dart
+++ _04_get_meals_app/lib/widgets/category_grid_item.dart
@@ -8,22 +8,28 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            category.color.withAlpha((0.55 * 255).toInt()),
-            category.color.withAlpha((0.9 * 255).toInt()),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () {},
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withAlpha((0.55 * 255).toInt()),
+              category.color.withAlpha((0.9 * 255).toInt()),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
-      ),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.titleLarge?.copyWith(
-          color: Theme.of(context).colorScheme.onSurface,
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge?.copyWith(
+            color: Theme.of(context).colorScheme.onSurface,
+          ),
         ),
       ),
     );
