0eddc91faab9fa58e022ba74054b50c190d4abee
sohail019 <shaikhhsohail0193@gmail.com>
2025-02-25 16:18:29 +0530

    Add FiltersScreen and MainDrawer; integrate filter navigation in TabsScreen

---
+++ _04_get_meals_app/lib/screens/filters.dart
@@ -0,0 +1,57 @@
+import 'package:_04_get_meals_app/screens/tabs.dart';
+import 'package:_04_get_meals_app/widgets/main_drawer.dart';
+import 'package:flutter/material.dart';
+
+class FiltersScreen extends StatefulWidget {
+  const FiltersScreen({super.key});
+
+  @override
+  State<FiltersScreen> createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  var _glutenFreeFilterSet = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Your Filters")),
+      drawer: MainDrawer(
+        onSelectedScreen: (identifier) {
+          Navigator.of(context).pop();
+          if (identifier == 'meals') {
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (ctx) => const TabsScreen()),
+            );
+          }
+        },
+      ),
+      body: Column(
+        children: [
+          SwitchListTile(
+            value: _glutenFreeFilterSet,
+            onChanged: (isChecked) {
+              setState(() {
+                _glutenFreeFilterSet = isChecked;
+              });
+            },
+            title: Text(
+              "Gluten-free",
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
+            subtitle: Text(
+              "Only include gluten-free meals.",
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 34, right: 22),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- _04_get_meals_app/lib/screens/tabs.dart
+++ _04_get_meals_app/lib/screens/tabs.dart
@@ -1,6 +1,8 @@
 import 'package:_04_get_meals_app/models/meal.dart';
 import 'package:_04_get_meals_app/screens/categories.dart';
+import 'package:_04_get_meals_app/screens/filters.dart';
 import 'package:_04_get_meals_app/screens/meals.dart';
+import 'package:_04_get_meals_app/widgets/main_drawer.dart';
 import 'package:flutter/material.dart';
 
 class TabsScreen extends StatefulWidget {
@@ -44,6 +46,15 @@ class _TabsScreenState extends State<TabsScreen> {
     });
   }
 
+  void _setScreen(String identifier) {
+    Navigator.of(context).pop();
+    if (identifier == 'filters') {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (ctx) => const FiltersScreen()),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget activePage = CategoriesScreen(
@@ -61,6 +72,7 @@ class _TabsScreenState extends State<TabsScreen> {
     }
     return Scaffold(
       appBar: AppBar(title: Text(activePageTitle)),
+      drawer: MainDrawer(onSelectedScreen: _setScreen),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
