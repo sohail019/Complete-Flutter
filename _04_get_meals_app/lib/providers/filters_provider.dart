d5a97e26e914b06c4e9f550cb0ddeaf8d5ccf0f0
sohail019 <shaikhhsohail0193@gmail.com>
2025-02-26 19:55:11 +0530

    Implement filteredMealsProvider for enhanced meal filtering; update MealDetailsScreen to reflect favorite status and streamline TabsScreen to utilize the new provider.

--- _04_get_meals_app/lib/providers/filters_provider.dart
+++ _04_get_meals_app/lib/providers/filters_provider.dart
@@ -1,3 +1,4 @@
+import 'package:_04_get_meals_app/providers/meals_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 enum Filter { glutenFree, lactoseFree, vegetarian, vegan }
@@ -25,3 +26,27 @@ final filtersProvider =
     StateNotifierProvider<FiltersNotifier, Map<Filter, bool>>(
       (ref) => FiltersNotifier(),
     );
+
+final filteredMealsProvider = Provider((ref) {
+  final meals = ref.watch(mealsProvider);
+  final activeFilters = ref.watch(filtersProvider);
+  return meals.where((meal) {
+    if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
+      return false;
+    }
+
+    if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+      return false;
+    }
+
+    if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
+      return false;
+    }
+
+    if (activeFilters[Filter.vegan]! && !meal.isVegan) {
+      return false;
+    }
+
+    return true;
+  }).toList();
+});
